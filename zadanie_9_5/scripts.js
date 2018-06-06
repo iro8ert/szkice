0b79bdfa11901cc31f8d16de5bcc54e5de123a47
Robert Nowaczyk <iro8ert@o2.pl>
2018-06-06 09:12:33 +0200

    add 9.5

--- zadanie_9_5/scripts.js
+++ zadanie_9_5/scripts.js
@@ -1,1 +1,11 @@
 console.log(window);
+
+//setTimeOut
+
+function raiseAlarm() {
+	alert('wiadomość co dwie sekundy czyli 2000 milisekund');
+}
+
+var delay = setTimeOut(raiseAlarm, 2000);
+//raiseAlarm(); // pamiętaj o wywowłaniu funkcji. bez tego nie za bardzo działa :) 
+
